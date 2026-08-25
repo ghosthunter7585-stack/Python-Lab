ff692429138ed1b9aebf2d0cd2edeeb01ac17971
Narendra <ghosthunter7585.com>
2026-08-25 11:18:40 +0530

    add lab26.py

--- Lab2Q6.py
+++ Lab2Q6.py
@@ -45,3 +45,42 @@ for row in range(1, n + 1):
         print("*", end="")
 
     print()
+
+
+# Pattern 4: Hollow Diamond 
+n = int(input("Enter an odd number: "))
+
+mid = n // 2
+
+for i in range(n):
+    if i <= mid:
+        stars = i
+    else:
+        stars = n - i - 1
+
+    spaces = mid - stars
+
+    print(" " * spaces, end="")
+
+    if stars == 0:
+        print("*")
+    else:
+        print("*" + " " * (2 * stars - 1) + "*")
+
+
+#Pattern 5: Butterfly Pattern
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+    spaces = 2 * (n - i)
+
+    print("*" * i, end="")
+    print(" " * spaces, end="")
+    print("*" * i)
+
+for i in range(n - 1, 0, -1):
+    spaces = 2 * (n - i)
+
+    print("*" * i, end="")
+    print(" " * spaces, end="")
+    print("*" * i)
